28d406c4ba9e8b18c7cc1f77dcf623d187661370
Gustavo Reichelt Emmel <gustavo.emmel@totvs.com.br>
2018-07-18 22:23:42 -0300

    association rules

--- fourth_class/fourth.py
+++ fourth_class/fourth.py
@@ -6,6 +6,9 @@ from scipy.cluster.hierarchy import fcluster
 import pandas as pd
 from sklearn.metrics import silhouette_score, calinski_harabaz_score
 
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+
 data = load_iris()
 df = pd.DataFrame(
     data['data'],
@@ -38,10 +41,10 @@ clusters = fcluster(z, k, criterion='maxclust')
 
 score_km = silhouette_score(df, labels)
 score_hi = silhouette_score(df, clusters)
-print(score_km)
-print(score_hi)
-print()
-print()
+# print(score_km)
+# print(score_hi)
+# print()
+# print()
 
 for k in xrange(2, 11):
     cl = KMeans(n_clusters=k, init='k-means++', n_init=10, max_iter=300)
@@ -49,4 +52,27 @@ for k in xrange(2, 11):
     labels = cl.labels_
     score_km = silhouette_score(df, labels)
     score_ca = calinski_harabaz_score(df, labels)
-    print k, score_km, score_ca
+    # print k, score_km, score_ca
+
+
+# regras de associacao
+
+df = pd.read_csv('Retail_Data.csv')
+df = df.iloc[:,1:]
+print df.head()
+
+df1 = pd.get_dummies(df)
+print df1.head()
+df1.shape
+
+frequent_items = apriori(df1, min_support=0.05, use_colnames=True)
+
+rules = association_rules(frequent_items, metric='lift', min_threshold=1)
+type(rules)
+rules.shape
+rules
+
+rules.sort_values('support', ascending= False).head(10)
+rules.sort_values('confidence', ascending= False).head(10)
+
+print rules
